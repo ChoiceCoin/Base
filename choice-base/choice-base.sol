c742ceefbffd6847f8b6c333fd9492353354fe66
Choice Coin <87402354+ChoiceCoin@users.noreply.github.com>
2024-12-30 13:48:47 -0800

    Add files via upload

---
+++ choice-base/choice-base.sol
@@ -0,0 +1,11 @@
+// contracts/Choice.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ChoiceCoiin is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Choice Coin", "Choice") {
+        _mint(msg.sender, 1000000000000000000000000000);
+    }
+}
